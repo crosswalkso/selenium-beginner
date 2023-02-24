66c130d5bd93c93cec7963391ac8ff66dfc7bc1a
crosswalk <crosswalk0628@gmail.com>
2023-02-24 22:18:17 +0900

    1.1 Searching

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
@@ -12,3 +14,11 @@ driver = webdriver.Chrome(
 )
 
 driver.get("https://google.com")
+
+search_bar = driver.find_element(By.CLASS_NAME, "gLFyf")
+search_bar.send_keys("hello!" + Keys.ENTER)
+
+search_results = driver.find_elements(By.CLASS_NAME, "g")
+
+print(search_results)
+# driver.quit()
