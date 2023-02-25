83781b2d62a988d09f18f06b0907f585effa0716
crosswalk <crosswalk0628@gmail.com>
2023-02-25 21:17:19 +0900

    1.3 Taking Screenshots

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+KEYWORD = "buy domain"
+
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 
@@ -17,13 +19,14 @@ driver.get("https://google.com")
 
 # 검색창, 검색
 search_bar = driver.find_element(By.CLASS_NAME, "gLFyf")
-search_bar.send_keys("hello!" + Keys.ENTER)
+search_bar.send_keys(KEYWORD + Keys.ENTER)
 
 # array
-search_results = driver.find_elements(By.CLASS_NAME, "g")
+search_results = driver.find_elements(By.CLASS_NAME, "uEierd")
 
-for search_result in search_results:
-    title = search_result.find_element(By.TAG_NAME, "h3")
-    if title:
-        print(title.text)
-# driver.quit()
+for index, search_result in enumerate(search_results):
+    search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
+
+
+# v5yQqb
+driver.quit()
