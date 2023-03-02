ec17ed0641fed29d4cfb09891026d2e2e8fa9675
crosswalk <crosswalk0628@gmail.com>
2023-03-02 13:31:09 +0900

    Deal Searching Part 1,2 Finished

--- codecamp/03_bot/booking/booking.py
+++ codecamp/03_bot/booking/booking.py
@@ -42,7 +42,7 @@ class Booking(webdriver.Chrome):
     def __exit__(self, exc_type, exc, traceback):
         # browser quit()을 teardown으로 관리
         if self.teardown:
-            time.sleep(2)
+            time.sleep(3)
             print("Browser successfuly ended.")
             self.quit()
 
@@ -57,3 +57,70 @@ class Booking(webdriver.Chrome):
             f"//*[@class='cf67405157']//*[text()='{currency}']",  # {currency} => EUR, not 'EUR'
         )
         selected_currency_element.click()
+
+    def select_place_to_go(self, place_to_go):
+        search_field = self.find_element(
+            By.CSS_SELECTOR,
+            'input[name="ss"]',
+        )
+        search_field.clear()
+        search_field.send_keys(place_to_go)
+        first_result = self.find_element(
+            By.XPATH,
+            f"//*[@class='f9a8ccdcc6']//*[text()='{place_to_go}']",
+        )
+        first_result.click()
+
+    def select_dates(self, check_in_date, check_out_date):
+        check_in_element = self.find_element(
+            By.CSS_SELECTOR,
+            f'span[data-date="{check_in_date}"]',
+        )
+        check_in_element.click()
+
+        check_out_element = self.find_element(
+            By.CSS_SELECTOR,
+            f'span[data-date="{check_out_date}"]',
+        )
+        check_out_element.click()
+
+    def select_adults(self, count=1):
+        selection_element = self.find_element(
+            By.CSS_SELECTOR,
+            'button[data-testid="occupancy-config"]',
+        )
+        selection_element.click()
+
+        while True:
+            decrease_adults_element = self.find_element(
+                By.XPATH,
+                f"//*[@class='df856d97eb']//*[@class='b2b5147b20'][1]//*[@class='e98c626f34']/button[1]",
+            )
+            decrease_adults_element.click()
+            # If the value of adults reaches 1, then we should get out
+            # of the while loop
+            adults_value_element = self.find_elements(
+                By.CSS_SELECTOR,
+                'span[class="e615eb5e43"]',
+            )[0]
+            adults_value = (
+                adults_value_element.text
+            )  # Should give back the adults count
+
+            if int(adults_value) == 1:
+                break
+
+        increase_adults_element = self.find_element(
+            By.XPATH,
+            f"//*[@class='df856d97eb']//*[@class='b2b5147b20'][1]//*[@class='e98c626f34']/button[2]",
+        )
+
+        for _ in range(count - 1):
+            increase_adults_element.click()
+
+    def click_search(self):
+        search_button = self.find_element(
+            By.CSS_SELECTOR,
+            'button[type="submit"]',
+        )
+        search_button.click()
--- codecamp/03_bot/run.py
+++ codecamp/03_bot/run.py
@@ -1,8 +1,12 @@
 import time
 from booking.booking import Booking
 
-search = input()
+# search = input()
 
 with Booking(teardown=True) as bot:
     # time.sleep(1)
-    bot.change_currency(search)
+    bot.change_currency("EUR")
+    bot.select_place_to_go("제주도")
+    bot.select_dates("2023-03-08", "2023-03-15")
+    bot.select_adults(5)
+    bot.click_search()
